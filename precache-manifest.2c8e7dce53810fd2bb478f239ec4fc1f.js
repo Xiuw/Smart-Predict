0962932b7249bcfe67f7a79d68401065eb133e54
xiuw <sylvi.xw@gmail.com>
2019-03-21 20:29:56 -0400

    Updates

---
+++ precache-manifest.2c8e7dce53810fd2bb478f239ec4fc1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f44f04f5f3736647dfe2",
+    "url": "/demographic-app/static/js/runtime~main.f44f04f5.js"
+  },
+  {
+    "revision": "129f46e03a6a7f0d8e38",
+    "url": "/demographic-app/static/js/main.129f46e0.chunk.js"
+  },
+  {
+    "revision": "d8cb8961bef3b11f1415",
+    "url": "/demographic-app/static/js/2.d8cb8961.chunk.js"
+  },
+  {
+    "revision": "129f46e03a6a7f0d8e38",
+    "url": "/demographic-app/static/css/main.fedec52b.chunk.css"
+  },
+  {
+    "revision": "d8cb8961bef3b11f1415",
+    "url": "/demographic-app/static/css/2.cef3fa15.chunk.css"
+  },
+  {
+    "revision": "33f5c5834307aa4634cd02ecbe47d0aa",
+    "url": "/demographic-app/index.html"
+  }
+];
